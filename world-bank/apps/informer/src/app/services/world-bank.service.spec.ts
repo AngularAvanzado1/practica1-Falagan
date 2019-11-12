da8ba90c1696e6fdbc399606d6433d3c6ee374d3
Eloy Perez <eloy.perez@igmweb.com>
2019-11-12 11:27:51 +0100

    Service test

--- world-bank/apps/informer/src/app/services/world-bank.service.spec.ts
+++ world-bank/apps/informer/src/app/services/world-bank.service.spec.ts
@@ -1,18 +1,45 @@
-import { TestBed } from '@angular/core/testing';
-import {
-  HttpClientTestingModule
-} from '@angular/common/http/testing';
+import { TestBed, async } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { WorldBankService } from './world-bank.service';
+import { Observable } from 'rxjs';
 
-describe('WorldBankService', () => {
-  beforeEach(() =>
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
-    })
-  );
+describe('GIVEN a WorldBankService', () => {
+  describe('WHEN: The DataModule is compiled', () => {
+    beforeEach(() =>
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        providers: [WorldBankService]
+      })
+    );
 
-  it('should be created', () => {
-    const service: WorldBankService = TestBed.get(WorldBankService);
-    expect(service).toBeTruthy();
+    it('THEN: should be created', () => {
+      const service: WorldBankService = TestBed.get(WorldBankService);
+      expect(service).toBeTruthy();
+    });
+
+    it(`THEN: should return an observable when call 'getContinentalRegions()'`, () => {
+      const service: WorldBankService = TestBed.get(WorldBankService);
+      const greetings$: Observable<any> = service.getContinentalRegions();
+      expect(greetings$).toBeInstanceOf(Observable);
+    });
+
+    // Ojo al async para ejectuar las llamadas asíncronas
+    it(`THEN: should return an Array so not being null when call 'getContinentalRegions()'`, async(() => {
+      const service: WorldBankService = TestBed.get(WorldBankService);
+      service
+        .getContinentalRegions()
+        .subscribe(result => expect(result).toBeNull());
+      // mock del backend para no depender del servidor
+      const httpMock = TestBed.get(HttpTestingController);
+      // esperar a que se llame a esta ruta
+      const req = httpMock.expectOne(
+        'http://api.worldbank.org/v2/region/?format=json'
+      );
+      // req.flush({ message: 'Welcome to api!' }); // responder con esto
+      httpMock.verify(); // comprobar que no hay más llmadas
+    }));
+
   });
+
+
 });
